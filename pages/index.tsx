b79d740aa27f64d3a602d94f1b09cc50039b4330
ryota.doi <ryota.doi.1102@gmail.com>
2021-10-25 09:41:00 +0900

    :art: add image tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ const Home: NextPage = () => {
       </header>
       <main>
         <h1>HACK THE PRECONCEIVED BIAS.</h1>
+        <img src="" alt="ファーストビュー"/>
         <p>W&amp;Dでは、潜在的な課題を浮き彫りにし、技術の力で企業及び事業の成長を支えます。</p> 
       </main>
       <footer>
